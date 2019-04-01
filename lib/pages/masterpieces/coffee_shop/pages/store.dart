099ef945b183a3a4322ca0e045b4badf9549ef53
Bobson Lin <bobson801104@gmail.com>
2019-04-01 16:22:19 +0800

    feat: User google_maps_flutter in coffee_shop app

--- lib/pages/masterpieces/coffee_shop/pages/store.dart
+++ lib/pages/masterpieces/coffee_shop/pages/store.dart
@@ -1,13 +1,67 @@
 import 'package:flutter/material.dart';
 
-class StorePage extends StatelessWidget {
+//import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+//import 'package:location/location.dart';
+
+class StorePage extends StatefulWidget {
+  @override
+  StorePageState createState() {
+    return new StorePageState();
+  }
+}
+
+class StorePageState extends State<StorePage> {
+  GoogleMapController mapController;
+
+//  LocationData currentLocation;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Store"),
       ),
-      body: Container(),
+      body: Container(
+//        child: Text(currentLocation.toString()),
+        child: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(25.071427, 121.367754),
+            zoom: 15,
+          ),
+        ),
+      ),
     );
   }
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+      mapController.addMarker(
+        MarkerOptions(
+          draggable: false,
+          position: LatLng(
+            25.0694622,
+            121.3630223,
+          ),
+          infoWindowText: InfoWindowText("Coffee Shop", "金鑛咖啡"),
+        ),
+      );
+    });
+  }
+
+//  _getCurrentPos() async {
+//    try {
+//      var location = new Location();
+//      currentLocation = await location.getLocation();
+//    } on PlatformException {
+//      currentLocation = null;
+//    }
+//  }
 }
